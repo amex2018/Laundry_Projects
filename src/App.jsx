6630cc299051400438b1b7a2e4f366e233c06776
Amanuel Wakshum <>
2022-11-16 13:03:41 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+export default function App() {
+  return (
+   <div className='Container'>
+    <nav className='Navbar'>
+       <span>Laundry App</span>
+     <ul>
+      <li>Home</li>
+       <li>Abouts</li>
+       <li>Blog</li>
+       <li>Contact us</li>
+     </ul>
+    </nav>
+   </div>
+  )
+}
